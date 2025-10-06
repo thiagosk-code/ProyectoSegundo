0335191634750f92cf1f10aae2eaa99c0171673a
juano1503 <juanoliceo@gmail.com>
2025-10-06 08:51:08 -0300

    todo
    todo los archivos d 0

---
+++ pruebaProyecto2/src/main/java/baseDeDatos/ConexionContra.java
@@ -0,0 +1,31 @@
+package baseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionContra {
+	
+	private crearBD cB = new crearBD();
+	
+	public Connection conectar() {
+		
+		Connection conn = null;
+		
+		try {
+			cB.crearBase();
+			String driver = "com.mysql.cj.jdbc.Driver";
+			Class.forName(driver);
+			String url = "jdbc:mysql://localhost:3306/Proyecto?useSSL=false&serverTimezone=UTC";
+			conn = DriverManager.getConnection(url,"root","57865578");
+			
+			
+		}catch (ClassNotFoundException e) {
+			System.out.print("Error en la conexión con el driver");
+		}catch (SQLException ex) {
+			System.out.print("Error en la conexión con la base");
+		}
+		return conn;
+		
+	}
+}
