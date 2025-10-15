3512d4c09101d7048734a2d41f79a09882bca612
TestUser1d <hojasrayadas100@gmail.com>
2025-10-15 13:40:57 -0300

    Update PartidasServlet.java 🐻 🦓 🦁
    El otorrinolaringólogo (ólogo)
    Como antes también el geólogo (ólogo)
    Y como luego el odontólogo (ólogo)
    Tomaron una decisión.
    Llamaron a Pepe el radiólogo (ólogo)
    Y a su compadre el entomólogo (ólogo)
    Y acompañados del cardiólogo (ólogo)
    Se fueron a bailar el son.
    Esto que ustedes bailan es Cachureos señor, es Cachureos
    Esto que ustedes bailan es Cachureos señor, es Cachureos
    El otorrino laringólogo (ólogo)
    Como antes también el geólogo (ólogo)
    Y como luego el odontólogo (ólogo)
    Tomaron una decisión.
    Llamaron a Pepe el radiólogo (ólogo)
    Y a su compadre el entomólogo (ólogo)
    Y acompañados del cardiólogo (ólogo)
    Se fueron a bailar el son.
    Esto que estan bailando es Caramelo señor, es Caramelo
    Esto que estan bailando es Caramelo es Caramelo tesoro, es Caramelo
    Esto que estan bailando es Caramelo es Cachureos señor, es Cachureos
    El otorrino laringólogo (ólogo)
    Como antes también el geólogo (ólogo)
    Y como luego el odontólogo (ólogo)
    Tomaron una decisión.
    Llamaron a Pepe el radiólogo (ólogo)
    Y a su compadre el entomólogo (ólogo)
    Y acompañados del cardiólogo (ólogo)
    Se fueron a bailar el son.

--- src/main/java/servlets/PartidasServlet.java
+++ src/main/java/servlets/PartidasServlet.java
@@ -21,12 +21,33 @@ public class PartidasServlet extends HttpServlet {
 
         String id_partidaStr = request.getParameter("idPartida");
        
-        String correo = (String) request.getSession().getAttribute("correoUsuario");
+        String correo = (String) request.getSession().getAttribute("correo");
 
         if (id_partidaStr == null || correo == null) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parámetros incompletos (ID de partida o usuario no encontrado en sesión).");
             return;
         }
+        int idPartida;
+        try {
+        	idPartida = Integer.parseInt(id_partidaStr);
+        	PersonajePartidaInfo detalles = null;
+        	detalles = partidaController.obtenerPartidaExistente(idPartida, correo);
+        	        
+        	if (detalles == null) {
+        		System.out.println("Partida slot ID " + idPartida + " no encontrada. Intentando crear una nueva...");
+        		detalles = partidaController.crearNuevaPartida(idPartida, correo);
+        	}
+        	
+        	if (detalles != null) {
+        		request.setAttribute("personajeInfo", detalles);
+        		request.getRequestDispatcher("/infoPartida.jsp").forward(request, response);
+        		
+        	}else {
+        		response.sendRedirect("error.jsp?msg=Error fatal: No se pudo caragr ni crear la partida. Verifique la conexión a BD.");
+        	}
+        }catch (NumberFormatException e) {
+        		response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID de partida no es un número válido");
+        	}
 
         try {
             int idPartida = Integer.parseInt(id_partidaStr);
